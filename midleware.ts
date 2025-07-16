fb46636850911696925a51cfce842ce2b4bda4de
haniparr <haniparohman@gmail.com>
2025-07-16 21:23:10 +0700

    Update midleware.ts

--- midleware.ts
+++ midleware.ts
@@ -1,35 +1,28 @@
+import { NextResponse } from "next/server";
+import { auth } from "./auth";
 
-import { auth } from "./auth"
- 
 export default auth((req) => {
-  const { nextUrl } = req
-  const isLoggedIn = !!req.auth?.user
-  
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth?.user;
 
-  const ProtectedRoutes = ["/dashboard"]
-  
+  const ProtectedRoutes = ["/dashboard"];
 
-  const isProtectedRoute = ProtectedRoutes.some(route => 
+  const isProtectedRoute = ProtectedRoutes.some((route) =>
     nextUrl.pathname.startsWith(route)
-  )
-  
+  );
 
   if (!isLoggedIn && isProtectedRoute) {
-    return Response.redirect(new URL("/login", nextUrl))
+    return NextResponse.redirect(new URL("/login", nextUrl));
   }
-  
 
   if (isLoggedIn && nextUrl.pathname.startsWith("/login")) {
-    return Response.redirect(new URL("/dashboard", nextUrl))
+    return NextResponse.redirect(new URL("/dashboard", nextUrl));
   }
-  
-  return null 
-})
 
+  // Return NextResponse.next() instead of null
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: [
- 
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
-}
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
