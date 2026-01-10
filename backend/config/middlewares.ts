f6a0359ec948aabfb157a94b0d79c64349a5e4e0
haniparr <haniparohman@gmail.com>
2026-01-10 13:25:10 +0700

    revert backend

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,47 +1,8 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'res.cloudinary.com',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'res.cloudinary.com',
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: [
-        'http://localhost:5173',
-        'http://localhost:3000',
-        process.env.CLIENT_URL,
-        // Add your Vercel URL after frontend deployment
-      ].filter(Boolean),
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-      headers: '*',
-      keepHeaderOnError: true,
-    },
-  },
+  'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
